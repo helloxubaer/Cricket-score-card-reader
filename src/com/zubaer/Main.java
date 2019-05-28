fb42bbc2dc675eb0684d9b5859f49e5f69bdc5cc
helloxubaer <xubaer.xeo@gmail.com>
2019-05-28 20:08:34 +0200

    final, chagned score card with full name

--- src/com/zubaer/Main.java
+++ src/com/zubaer/Main.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        MyTeam teamXubaer = new MyTeam("Team_xubaer");
+        MyTeam teamXubaer = new MyTeam("Player_in_all_team");
         teamXubaer.calculateAll();
+        //teamXubaer.playerListRead();
+        //teamXubaer.runCalculate();
+        //teamXubaer.wicket();
+        //teamXubaer.catchCalculate();
+        //teamXubaer.extraPoint();
+        //teamXubaer.pointWrite();
 
 
     }
--- src/com/zubaer/MyTeam.java
+++ src/com/zubaer/MyTeam.java
@@ -8,30 +8,32 @@ public class MyTeam {
     private static int teamPoint;
     private String teamName;
 
-
     public MyTeam(String teamName) {
         this.teamName = teamName;
     }
 
-    public void calculateAll(){
+    public void calculateAll() {
         playerListRead();
-        pointCalculate();
-        manualPointInput();
+        runCalculate();
+        wicket();
+        catchCalculate();
+        extraPoint();
         pointWrite();
     }
 
     public void playerListRead() {
-        //player.clear();
+
         Scanner scanner = null;
         try {
-            scanner = new Scanner(new FileReader(getTeamName()+".csv"));
+            scanner = new Scanner(new FileReader("Player_in_all_team.csv"));
             scanner.useDelimiter(";");
             while (scanner.hasNextLine()) {
                 String name = scanner.next().trim();
-                scanner.skip(scanner.delimiter());
                 String role = scanner.nextLine().trim();
                 Player createPlayer = new Player(name, role);
                 player.put(name, createPlayer);
+                //System.out.println(createPlayer);
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -43,47 +45,42 @@ public class MyTeam {
     }
 
 
-    public void pointCalculate() {
+    public void runCalculate() {
 
-//search run
         Scanner scanner = null;
         try {
             scanner = new Scanner(new FileReader("ScoreCard.csv"));
             while (scanner.hasNextLine()) {
-                String[] line1 = scanner.nextLine().split(";");
-                String[] line2 = scanner.nextLine().split(";");//wicket and catch info
-                String[] line3 = scanner.nextLine().split(";");
-                String name = line1[1].trim();
-                System.out.println(name);
-                for (String key : player.keySet()) {
-                    if (name.trim().equalsIgnoreCase(key.trim())) {
-                        int run = Integer.parseInt(line3[1].trim());
-                        Player player1 = player.get(name.trim());
-                        try {
-                            player1.addPoint(run / 5);
-                            int multiplier = 10;
-                            int count = 0;
-                            int bonus = 0;
-                            for (int i = 50; i <= run; i = i + 50) {
-                                count++;
-                            }
-                            for (int i = 1; i < count; i++) {
-                                multiplier += 5;
-                            }
-                            bonus = count * multiplier;
-                            player1.addPoint(bonus);
+                String[] line1 = scanner.nextLine().split(";");//run info
+                if (line1.length == 6) {
+                    String name = line1[0].trim();
+                    for (String key : player.keySet()) {
+                        if (name.trim().equalsIgnoreCase(key.trim())) {
+                            int run = Integer.parseInt(line1[1].trim());
+                            Player player1 = player.get(name.trim());
+                            try {
+                                player1.addPoint(run / 5);
+                                int multiplier = 10;
+                                int count = 0;
+                                int bonus = 0;
+                                for (int i = 50; i <= run; i = i + 50) {
+                                    count++;
+                                }
+                                for (int i = 1; i < count; i++) {
+                                    multiplier += 5;
+                                }
+                                bonus = count * multiplier;
+                                player1.addPoint(bonus);
 
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
                 }
-                for (int i = 0; i < 5; i++) {
-                    if (scanner.hasNextLine()) {
-                        scanner.nextLine();
-                    }
-                }
             }
+
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -92,28 +89,33 @@ public class MyTeam {
             }
         }
 
-//search wicket:
+    }
+
+    public void wicket() {
+        Scanner scanner = null;
         try {
-            scanner = new Scanner(new FileReader("wicket.csv"));
-            scanner.useDelimiter(",");
+            scanner = new Scanner(new FileReader("ScoreCard.csv"));
+
             while (scanner.hasNextLine()) {
                 String[] line1 = scanner.nextLine().split(";");
-                String name = line1[0].trim();
-                for (String key : player.keySet()) {
-                    if (name.trim().equalsIgnoreCase(key.trim())) {
-                        int wicket = Integer.parseInt(line1[1].trim());
-                        Player player1 = player.get(name);
-                        int wicketPoint = 0;
-                        if (wicket < 5) {
-                            wicketPoint = wicket * 5;
-                        }
-                        if (wicket >= 5) {
-                            wicketPoint += (25 + (5 * 5) + (wicket - 5) * 10);
-                        }
-                        //System.out.println(wicketPoint);
-                        player1.addPoint(wicketPoint);
-                        if (Double.parseDouble(line1[2]) <= 3.5) {
-                            player1.addPoint(10);
+
+                if (line1.length == 8) {
+                    String name = line1[0].trim();
+                    for (String key : player.keySet()) {
+                        if (name.trim().equalsIgnoreCase(key.trim())) {
+                            int wicket = Integer.parseInt(line1[4].trim());
+                            Player player1 = player.get(name);
+                            int wicketPoint = 0;
+                            if (wicket < 5) {
+                                wicketPoint = wicket * 5;
+                            }
+                            if (wicket >= 5) {
+                                wicketPoint += (25 + (5 * 5) + (wicket - 5) * 10);
+                            }
+                            player1.addPoint(wicketPoint);
+                            if (Double.parseDouble(line1[7]) <= 3.5) {
+                                player1.addPoint(10);
+                            }
                         }
                     }
                 }
@@ -125,32 +127,50 @@ public class MyTeam {
                 scanner.close();
             }
         }
+    }
 
-//search catch:
+    public void catchCalculate() {
         Scanner scanner2 = null;
+
         try {
             scanner2 = new Scanner(new FileReader("ScoreCard.csv"));
             while (scanner2.hasNextLine()) {
-
-                String[] line1 = scanner2.nextLine().split(";");
-                String[] line2 = scanner2.nextLine().split(";");//catch and bowl info
-                String[] info = line2[1].split(" ");
-                int length = info.length;
                 String str = "";
-                if (info[0].trim().equalsIgnoreCase("c") || info[0].trim().equalsIgnoreCase("st")) {
-                    for (int i = 1; i < length; i++) {
-                        if (!(info[i].trim().equalsIgnoreCase("b"))) {
-                            str = str + " " + info[i].trim();
-                            str=str.replace("�","");
-                        } else if ((info[i].trim().equalsIgnoreCase("b"))) {
-                            break;
+                String[] line1 = scanner2.nextLine().split(";");
+                if (line1.length == 1) {
+                    String[] info = line1[0].trim().split(" ");
+                    int length = info.length;
+                    //c
+                    if ((info[0].trim().equalsIgnoreCase("c") ||
+                            info[0].trim().equalsIgnoreCase("st")) && !info[1].trim().equalsIgnoreCase("&")) {
+                        for (int i = 1; i < length; i++) {
+                            if (!(info[i].trim().equalsIgnoreCase("b"))) {
+                                str = str + " " + info[i].trim();
+                            } else if ((info[i].trim().equalsIgnoreCase("b"))) {
+                                break;
+                            }
                         }
                     }
+                    //c & b
+                    if ((info[0].trim().equalsIgnoreCase("c") ||
+                            info[0].trim().equalsIgnoreCase("st")) && info[1].trim().equalsIgnoreCase("&")) {
+                        for (int i = 3; i < length; i++) {
+                            str = str + " " + info[i].trim();
+                        }
+                    }
+                    //run out:
+                    if (info[0].trim().equalsIgnoreCase("run")) {
+                        for (int i = 2; i < length; i++) {
+                            str = str + " " + info[i].trim();
+                        }
+
+                        str = str.replace("(", "");
+                        str = str.replace(")", "");
+                    }
                 }
-                System.out.println(str);
+
 
                 for (String key : player.keySet()) {
-
                     if (str.trim().equalsIgnoreCase(key.trim())) {
                         Player player1 = player.get(str.trim());
                         //except wK:
@@ -161,11 +181,7 @@ public class MyTeam {
                         }
                     }
                 }
-                for (int i = 0; i < 6; i++) {
-                    if (scanner2.hasNextLine()) {
-                        scanner2.nextLine();
-                    }
-                }
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -176,17 +192,35 @@ public class MyTeam {
         }
     }
 
+    public void manualInput(String playerName, int point){
+        for (String key : player.keySet()) {
+            if (playerName.trim().equalsIgnoreCase(key.trim())) {
+                Player player1 = player.get(playerName);
+                player1.addPoint(point);
+            }
+        }
+        
+    }
+
 
     public void pointWrite() {
         File file = new File(teamName.trim());
         FileWriter fw = null;
-        int matchCount =0;
+
+
+
         try {
-            fw = new FileWriter(file + "_points" + ".csv", true);
+            fw = new FileWriter(file + "_points" + ".csv");
+
             for (Player player : player.values()) {
-                teamPoint += player.getTotalPoint();
+                //teamPoint += player.getTotalPoint();
                 System.out.println("player: " + player.getPlayerName().trim() + " gives " + player.getTotalPoint() + " points");
-                fw.append(("player: " + player.getPlayerName() + " gives " + player.getTotalPoint() + " points ") + "\n");
+                //fw.append(("player: " + player.getPlayerName() + " gives " + player.getTotalPoint() + " points ") + ';'+"\n");
+                fw.append(player.getPlayerName());
+                fw.append(";");
+                fw.append(String.valueOf(player.getTotalPoint()));
+                fw.append("\n");
+
                 System.out.println();
             }
             fw.append("team points:" + "\n");
@@ -198,35 +232,51 @@ public class MyTeam {
         }
     }
 
-    public void manualPointInput() {
-        Scanner scanner3 = null;
-
+    public void extraPoint() {
+        Scanner scanner = null;
         try {
-            scanner3 = new Scanner(new FileReader("ExtraPoint.csv"));
-            while (scanner3.hasNextLine()) {
-                String[] name = scanner3.nextLine().split(";");
-                String catagory = name[0].trim();
-                String playerName = name[1].trim();
-                for (String key : player.keySet()) {
-                    if (playerName.trim().equalsIgnoreCase(key.trim())) {
-                        Player player1 = player.get(playerName.trim());
-                        if (catagory.equalsIgnoreCase("MOM")) {
+            scanner = new Scanner(new FileReader("ScoreCard.csv"));
+            while (scanner.hasNextLine()) {
+                String[] line1 = scanner.nextLine().split(";");//run info
+                if (line1.length == 2 && line1[0].trim().equalsIgnoreCase("MOM")) {
+                    String name = line1[1].trim();
+
+                    for (String key : player.keySet()) {
+                        if (name.trim().equalsIgnoreCase(key.trim())) {
+                            Player player1 = player.get(name.trim());
                             player1.addPoint(50);
-                        } else if (catagory.equalsIgnoreCase("RUN/OUT")) {
-                            int numberOfRunOut = Integer.parseInt(name[2]);
-                            player1.addPoint(2 * numberOfRunOut);
-                        } else if (catagory.equalsIgnoreCase("HATTRIC")) {
-                            player1.addPoint(10);
-                        } else if (catagory.equalsIgnoreCase("MOT")) {
-                            player1.addPoint(100);
                         }
                     }
                 }
+                if (line1.length == 2 && line1[0].trim().equalsIgnoreCase("Hattric")) {
+                    String name = line1[1].trim();
 
+                    for (String key : player.keySet()) {
+                        if (name.trim().equalsIgnoreCase(key.trim())) {
+
+                            Player player1 = player.get(name.trim());
+                            player1.addPoint(10);
+                        }
+                    }
+                }
+                if (line1.length == 2 && line1[0].trim().equalsIgnoreCase("MOT")) {
+                    String name = line1[1].trim();
+
+                    for (String key : player.keySet()) {
+                        if (name.trim().equalsIgnoreCase(key.trim())) {
+
+                            Player player1 = player.get(name.trim());
+                            player1.addPoint(200);
+                        }
+                    }
+                }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
         }
     }
 
